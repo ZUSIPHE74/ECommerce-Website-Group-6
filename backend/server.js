fd33f8fb9d09cf82adb2866dff37280c66a41cba
Tamrynripepi00 <tamrynripepi00@gmail.com>
2026-02-12 16:24:02 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import pool from './database.js';
+import pool from './config/database.js';
 
 dotenv.config();
 
